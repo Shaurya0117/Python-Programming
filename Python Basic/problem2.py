a8d3ccd584f24535158192f6c77d244c5f172274
Shaurya0117 <shaurya7185@gmail.com>
2026-06-22 21:38:59 +0530

    problems

--- Python Basic/problem2.py
+++ Python Basic/problem2.py
@@ -1,3 +1,4 @@
 #WAP to input side of a square and print its area
 
-side = int(input("Enter side o"))
+side = int(input("Enter side of square : "))
+print("Area of square is ", side*side)
---
+++ Python Basic/problem3.py
@@ -0,0 +1,5 @@
+#WAP to input 2 floating point numbers and prinnt there avg
+
+a = float(input("Enter the float num 1 : "))
+b = float(input("Enter the float num 2 : "))
+print("Average of 2 nums are ", (a+b)/2 )
