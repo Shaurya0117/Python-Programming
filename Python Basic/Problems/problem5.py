462dff1cb1bf2575424767b4e77d56c3f10d85ab
Shaurya0117 <shaurya7185@gmail.com>
2026-06-29 23:34:18 +0530

    problems relaed to string

---
+++ Python Basic/Problems/problem5.py
@@ -0,0 +1,8 @@
+#WAP to input user's first name & print its length
+str = input("Enter your name : ")
+print("Your first namer is " + str)
+print("length of your name is " , len(str))
+
+#WAP to input occurance of "s" in given string
+str = input("Enter ths string : ")
+print(str.count("s"))
--- Python Basic/string.py
+++ Python Basic/string.py
@@ -1,6 +1,6 @@
 #Concatenation
-str1 = "hello guys this is shaurya "
-str2 = "im learing python tODAY"
+str1 = "Hello guys this is Shaurya"
+str2 = "i'm learing python today"
 finalstr = str1 + " " + str2
 print(finalstr)
 
@@ -9,5 +9,19 @@ len(str1)
 print(len(str1))
 
 #indexing
-str = "Shauryaa singh"
-print(str[8])
+str = "Shauryaa Singh"
+print(str[9])
+
+#Slicing (Accessing parts of  string)
+str = "ABES Engineering College"
+print(str[5:16])
+print(str[5:len(str)])
+print(str[-7:-1])
+
+"""String Function"""
+str = "i'm studying python from kaggle and youtube"
+print(str.endswith("tube"))
+print(str.capitalize())                    #Capitalize first letter
+print(str.replace("python","javascript"))
+print(str.find("from"))                    #find 1st occurence
+print(str.count("o"))
