968329694628ea9f1ce8fa6d907167ebea983d5c
Shaurya0117 <shaurya7185@gmail.com>
2026-06-29 09:16:56 +0530

    string & problem

---
+++ Python Basic/string.py
@@ -0,0 +1,13 @@
+#Concatenation
+str1 = "hello guys this is shaurya "
+str2 = "im learing python tODAY"
+finalstr = str1 + " " + str2
+print(finalstr)
+
+#Length of str
+len(str1)
+print(len(str1))
+
+#indexing
+str = "Shauryaa singh"
+print(str[8])
